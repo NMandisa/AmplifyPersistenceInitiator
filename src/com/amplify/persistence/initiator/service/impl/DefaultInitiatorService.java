a6291d0655b72f90f0ef4481a4c63bce77248293
Noxolo.Mkhungo <Senamile95>
2017-04-15 12:47:39 +0200

    Commit-2017-04-04

---
+++ src/com/amplify/persistence/initiator/service/impl/DefaultInitiatorService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.amplify.persistence.initiator.service.impl;
+
+/**
+ *
+ * @author Noxolo.Mkhungo
+ */
+public class DefaultInitiatorService {
+    
+}
