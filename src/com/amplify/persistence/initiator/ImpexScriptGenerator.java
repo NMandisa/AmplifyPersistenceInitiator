0b5aeff65dd697df285ab79b1bfd2e74fdf5d6dc
Noxolo.Mkhungo <Senamile95>
2017-04-15 12:31:28 +0200

    Commit-2017-04-04

--- src/com/amplify/persistence/initiator/ImpexScriptGenerator.java
+++ src/com/amplify/persistence/initiator/ImpexScriptGenerator.java
@@ -9,7 +9,6 @@ import com.amplify.persistence.initiator.model.MediaContainerValue;
 import com.amplify.persistence.initiator.model.MediaFolderValue;
 import com.amplify.persistence.initiator.model.MediaValue;
 import com.amplify.persistence.initiator.model.ProductValue;
-import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -24,7 +23,7 @@ public class ImpexScriptGenerator {
      * @param args the command line arguments
      */
     //Delimiter used in CSV file
-    private static final String commaDelimiter = ";";
+    private static final String COMMA_DELIMITER = ";";
     private static final String newLineSeparator = "\n";
 
     public static void main(String[] args) {
@@ -42,12 +41,12 @@ public class ImpexScriptGenerator {
         //  Populating the values for the Impex
         String filename = "HelloImAFile.jpg";
         String mediasConcatenate = "";//the Initailizing the concatenation of the file 
-        //Expected Outcome --> /1200Wx1200H/EOH_089_1200.jpg,/300Wx300H/EOH_089_1200.jpg,/96Wx96H/EOH_089_1200.jpg,/515Wx515H/EOH_089_1200.jpg,/65Wx65H/EOH_089_1200.jpg,/30Wx30H/EOH_089_1200.jpg
+        //Expected Outcome --> /1200Wx1200H/HelloWorldIamAFile.jpg,/300Wx300H/HelloWorldIamAFile.jpg,/96Wx96H/HelloWorldIamAFile.jpg,/515Wx515H/HelloWorldIamAFile.jpg,/65Wx65H/HelloWorldIamAFile.jpg,/30Wx30H/HelloWorldIamAFile.jpg
         ProductValue productValueObject = new ProductValue();
         MediaFolderValue mfv = new MediaFolderValue();
         mfv.setPath("images"); mfv.setQualifier("images");
         //Expected Output --> ;images;images;
-        String mfvStringLine = commaDelimiter + mfv.getQualifier()+commaDelimiter+mfv.getPath();
+        String mfvStringLine = COMMA_DELIMITER + mfv.getQualifier()+COMMA_DELIMITER+mfv.getPath();
         System.out.println("#MEDIA");
         System.out.println(HEADER_STRINGS[0]);
         System.out.println("Media Folder Value Line : "+mfvStringLine);
@@ -76,13 +75,13 @@ public class ImpexScriptGenerator {
                 tempMediaContainerValueObject.setMedias(mediasConcatenate.substring(0, mediasConcatenate.length() - 1));//Removing the last string which is not needed
                 //Expected Outcome Example Below--
                 //1200Wx1200H/HelloWorldImAFile.jpg,/300Wx300H/HelloWorldImAFile.jpg,/96Wx96H/HelloWorldImAFile.jpg,/515Wx515H/HelloWorldImAFile.jpg,/65Wx65H/HelloWorldImAFile.jpg,/30Wx30H/HelloWorldImAFile.jpg
-                String mediaContainerValueLine = commaDelimiter+ tempMediaContainerValueObject.getQualifier() + commaDelimiter + tempMediaContainerValueObject.getMedias() + commaDelimiter + " " + commaDelimiter;
+                String mediaContainerValueLine = COMMA_DELIMITER+ tempMediaContainerValueObject.getQualifier() + COMMA_DELIMITER + tempMediaContainerValueObject.getMedias() + COMMA_DELIMITER + " " + COMMA_DELIMITER;
                 System.out.println("# Create Media Containers");
                 System.out.println(HEADER_STRINGS[2]);
                 System.out.println("Media Container Value : " + mediaContainerValueLine);
                 //Expected Outcome Example Below--
                 //;HelloWorldImAFile;;HelloWorldImAFile.jpg;/300Wx300H/HelloWorldImAFile.jpg;/96Wx96H/HelloWorldImAFile.jpg;
-                String productValueLine = commaDelimiter +"\t"+ productValueObject.getCode() + commaDelimiter + " " + commaDelimiter + productValueObject.getGalleryImages() + commaDelimiter + " " + productValueObject.getPicture() + " " + commaDelimiter + " " + productValueObject.getThumbnail();
+                String productValueLine = COMMA_DELIMITER +"\t"+ productValueObject.getCode() + COMMA_DELIMITER + " " + COMMA_DELIMITER + productValueObject.getGalleryImages() + COMMA_DELIMITER + " " + productValueObject.getPicture() + " " + COMMA_DELIMITER + " " + productValueObject.getThumbnail();
                 System.out.println("# Update Products with Media and Media Containers");
                 System.out.println(HEADER_STRINGS[3]);
                 System.out.println("Product Value Line : " + productValueLine);
@@ -105,7 +104,7 @@ public class ImpexScriptGenerator {
         System.out.println(HEADER_STRINGS[0]);
         while (iteratorMediaValues.hasNext()) {//the Object
             mediaValue = (MediaValue) iteratorMediaValues.next();
-            String valueLine = commaDelimiter+ mediaValue.getMediaFormat() + commaDelimiter + mediaValue.getCode() + " " + commaDelimiter + " " + mediaValue.getMedia() + " " + commaDelimiter + mediaValue.getMime() + commaDelimiter + mediaValue.getCatalogVersion() + " " + commaDelimiter + mediaValue.getFolder() + " " + commaDelimiter + mediaValue.getRealFilename();
+            String valueLine = COMMA_DELIMITER+ mediaValue.getMediaFormat() + COMMA_DELIMITER + mediaValue.getCode() + " " + COMMA_DELIMITER + " " + mediaValue.getMedia() + " " + COMMA_DELIMITER + mediaValue.getMime() + COMMA_DELIMITER + mediaValue.getCatalogVersion() + " " + COMMA_DELIMITER + mediaValue.getFolder() + " " + COMMA_DELIMITER + mediaValue.getRealFilename();
             System.out.println("Media Value Line : " + valueLine);
 
         }
